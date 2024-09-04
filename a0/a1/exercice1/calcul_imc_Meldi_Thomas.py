11a95eff2ab5ece0ba3aa22c93391bfbb8984ad0
ThomasF22 <tfurfaro9@gmail.com>
2024-09-04 14:36:23 +0200

    Atelier 1 - Exercice 1

---
+++ a0/a1/exercice1/calcul_imc_Meldi_Thomas.py
@@ -0,0 +1,33 @@
+import random 
+
+IMC = {'0': 'Rien',
+    '16.5': 'Dénutrition ou famine',
+       '18.5': 'maigreur',
+       '25': 'corpulence normale',
+       '30': 'surpoids',
+       '35': 'obésité modéré',
+       '40': 'obésité sévère',
+       '100': 'obésité morbide'}
+
+
+def message_imc(imcUtilisateur:float)->str:
+    """Prend en paramètre un imc et renvoie son interprétation"""
+    listeDesImc=list(IMC.keys())
+    count = 1
+    start = True
+    message = ''
+    for e in IMC:
+        if float(listeDesImc[count - 1]) <= imcUtilisateur < float(listeDesImc[count]) and start == True:
+            start = False
+            message = IMC.get(listeDesImc[count])
+        count += 1
+    return message
+#print(message_imc(14))
+
+def test_imc():
+    """Génère aléatoirement 10 IMC pour tester la fonction message_imc"""
+    for i in range(0,10):
+        imcRandom = random.randrange(0,100)
+        print('Pour une IMC de : ', imcRandom , ' Vous êtes en : ', message_imc(imcRandom))
+
+test_imc()
--- a0/exercice2/montant_affranchissement.py
+++ a0/exercice2/montant_affranchissement.py
@@ -1,3 +1,5 @@
+# à ajouter : dictionnaires, constantes, lambda peut-être ?
+
 def montantAff()-> int:
     tarif = 0
     poids = 0 
@@ -5,6 +7,7 @@ def montantAff()-> int:
     poidsMaxVerte = 3000
     poidsMaxPrioritaire = 3000
     poidsMaxEcopli = 30000
+    stickerSuivi = 0.5
     typeLettre = input('Entrez le type de la lettre (verte, prioritaire, ecopli)')
 
     if typeLettre == 'verte' or typeLettre == 'prioritaire' or typeLettre == 'ecopli':
@@ -17,52 +20,70 @@ def montantAff()-> int:
     # Lettre verte
 
     if typeLettre == 'verte':
-        count=0
+        demanderSticker = input('Sticker suivi ? O/N')
+        if demanderSticker == 'O':
+            print('tarif avant sticker : ', tarif)
+            tarif += 0.50
+            print('tarif après : ', tarif)
+        count=0 # compteur pour parcourir la liste
+
         # Poids et tarifs du type
         listePoids = [20,100,250,500,1000,3000]
         listeTarifs = [116,232,400,600,750,1050]
         if poids > poidsMaxVerte:
             print('Poids maximal atteint pour ce type de lettre!')
-        if poids < listePoids[0]:
-            return print('Tarif : ' , listeTarifs[0] / 100 , ' euros')
+
         for e in listePoids :
-            if poids < listePoids[count]:
-                return print('Tarif : ' , listeTarifs[count] / 100 , ' euros')
             count += 1
-        return print('Tarif : ', listeTarifs[count - 1] / 100 , ' euros')
+            if poids < listePoids[count - 1]:
+                tarif += (listeTarifs[count - 1])
+                break
+        return tarif
     
     # Lettre prioritaire
 
-    elif typeLettre == 'prioritaire':
-        count=0
+    if typeLettre == 'prioritaire':
+        demanderSticker = input('Sticker suivi ? O/N')
+        if demanderSticker == 'O':
+            print('tarif avant sticker : ', tarif)
+            tarif += 0.50
+            print('tarif après : ', tarif)
+        count=0 # compteur pour parcourir la liste
+
         # Poids et tarifs du type
-        listePoids = [20,100,250,500,3000]
+        listePoids = [20,100,250,500,1000,3000]
         listeTarifs = [143,286,526,789,1144]
         if poids > poidsMaxPrioritaire:
             print('Poids maximal atteint pour ce type de lettre!')
-        if poids < listePoids[0]:
-            return print('Tarif : ' , listeTarifs[0] / 100 , ' euros')
+
         for e in listePoids :
-            if poids < listePoids[count]:
-                return print('Tarif : ' , listeTarifs[count] / 100 , ' euros')
             count += 1
-        return print('Tarif : ', listeTarifs[count - 1] / 100 , ' euros')
+            if poids < listePoids[count - 1]:
+                tarif += (listeTarifs[count - 1])
+                break
+        return tarif
 
     # Lettre éco-pli
-    elif typeLettre == 'ecopli':
-        count=0
+    if typeLettre == 'ecopli':
+        demanderSticker = input('Sticker suivi ? O/N')
+        if demanderSticker == 'O':
+            print('tarif avant sticker : ', tarif)
+            tarif += 0.50
+            print('tarif après : ', tarif)
+        count=0 # compteur pour parcourir la liste
+
         # Poids et tarifs du type
         listePoids = [20,100,250]
         listeTarifs = [114,228,392]
         if poids > poidsMaxEcopli:
             print('Poids maximal atteint pour ce type de lettre!')
-        if poids < listePoids[0]:
-            return print('Tarif : ' , listeTarifs[0] / 100 , ' euros')
+
         for e in listePoids :
-            if poids < listePoids[count]:
-                return print('Tarif : ' , listeTarifs[count] / 100 , ' euros')
             count += 1
-        return print('Tarif : ', listeTarifs[count - 1] / 100 , ' euros')
+            if poids < listePoids[count - 1]:
+                tarif += (listeTarifs[count - 1])
+                break
+        return tarif
 
 
 
