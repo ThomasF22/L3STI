d09da3fc1d0622f3113361cbb648347510219a5c
ThomasF22 <tfurfaro9@gmail.com>
2024-09-04 16:39:43 +0200

    exercice 3

--- a0/a1/exercice1/calcul_imc_Meldi_Thomas.py
+++ a0/a1/exercice1/calcul_imc_Meldi_Thomas.py
@@ -30,13 +30,14 @@ def test_imc():
         imcRandom = random.randrange(0,100)
         print('Pour une IMC de : ', imcRandom , ' Vous êtes en : ', message_imc(imcRandom))
 
-test_imc()
+#test_imc()
 
 
 
 # Exercice 2
 
 def est_bissextile(annee:int)-> bool:
+    """Vérifie si l'année entrée est bissextile"""
     if annee % 4 == 0 and annee % 100 != 0:
         return True
     elif annee % 400 == 0:
@@ -45,7 +46,52 @@ def est_bissextile(annee:int)-> bool:
         return False
 
 def test_bissextile():
+    """Test la fonction est_bissextile()"""
     print(est_bissextile(2000))
     print(est_bissextile(2100))
     print(est_bissextile(2024))
 # test_bissextile()
+
+# Exercice 3
+
+def discriminant(a:float,b:float,c:float)->float:
+    """Calcule le discriminant d'une équation du second degré ax²+bx+c=0"""
+    delta=b*b - 4*a*c
+    return delta
+
+def racine_unique(a:float,b:float)->float:
+    """Calcule la racine unique d'une équation du second degré"""
+    x = -b / (2*a)
+    return x
+
+def racine_double(a:float,b:float,delta:float,num:int)-> float:
+    """Calcule la racine double d'une équation du second degré"""
+    if num == 1:
+        resultat = (- b + math.sqrt(delta)) / (2*a)
+    elif num == 2:
+        resultat = (- b - math.sqrt(delta)) / (2*a)
+    else:
+        print('Veuillez entrer un numéro de racine entre 1 et 2')
+    return resultat
+
+def str_equation(a:float,b:float,c:float)-> str:
+    """Renvoie l'équation du second degré en chaîne de caractères"""
+    return '{}x² + {}x + {} =   0'.format(a,b,c)
+
+
+def solution_equation(a:float,b:float,c:float)-> str:
+    """Résoud une équation du second degré"""
+    message = ''
+    delta=discriminant(a,b,c)
+    if delta < 0:
+        message = 'Solution de l\'équation {} \n Pas de racine réelle'.format(str_equation(a,b,c))
+    elif delta == 0:
+        message = 'Solution de l\'équation {} \n Racine unique : x = {}'.format(str_equation(a,b,c),racine_unique(a,b))
+    else: 
+        message = '"Solution de l\'équation {} \n Deux racines : \n x1 = {} \n x2 = {}'.format(str_equation(a,b,c),racine_double(a,b,delta,1)
+        ,racine_double(a,b,delta,2))
+    return message
+
+#print(solution_equation(2,1,0))
+
+#def equation(a:int,b:int,c:int):
